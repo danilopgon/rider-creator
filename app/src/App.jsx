7bd9fed136c450533f4811356f83d331943d5474
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-07-18 15:22:11 +0000

    elige rol

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,11 +5,7 @@ import { LoginProvider } from "./context/LoginContext";
 import NavBar from "./components/NavBar";
 import Home from "./views/Home";
 import Login from "./views/Login";
-<<<<<<< HEAD
 import RiderCreation from "./views/RiderCreation";
-=======
-import Dashboard from "./views/Dashboard";
->>>>>>> a7c5c23fc54a11ef4c4f8266b8646367245e6750
 
 function App() {
   return (
@@ -19,12 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/rider_creation" element={<RiderCreation />} />
-=======
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="*" element={<h1>Not Found</h1>}></Route>
->>>>>>> a7c5c23fc54a11ef4c4f8266b8646367245e6750
         </Routes>
       </LoginProvider>
     </BrowserRouter>
