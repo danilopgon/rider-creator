65aaf6048468ca6ca8befb914acd5aaa8a1cd1df
Daniel López González <dani.lopezgonzalez.94@gmail.com>
2023-07-18 20:31:15 +0200

    fix login problems

--- app/src/services/login.js
+++ app/src/services/login.js
@@ -1,8 +1,6 @@
 const login = async (userInfo) => {
   const { email, password } = userInfo;
 
-  console.log(userInfo);
-
   try {
     const response = await fetch(
       `${import.meta.env.VITE_API_URL}api/auth/login`,
@@ -14,6 +12,7 @@ const login = async (userInfo) => {
     );
     const data = await response.json();
     localStorage.setItem("jwt-token", data.token);
+    return response;
   } catch (error) {
     console.log(error);
   }
