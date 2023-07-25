16a5e1b5102533267bec261fbea94f9cc965f2ca
Daniel López González <dani.lopezgonzalez.94@gmail.com>
2023-07-25 13:25:19 +0200

    Fix recovery text

--- api/templates_email/recover_password.py
+++ api/templates_email/recover_password.py
@@ -4,9 +4,8 @@ import os
 def email_recovery_text(username="", url=""):
     return f""" 
     Estimado/a {username}
-    ¡Bienvenido/a a [Nombre de la empresa/organización]!
-    Estamos encantados de que hayas decidido unirte a nuestra comunidad. Para comenzar a aprovechar al máximo nuestros servicios, es necesario que actives tu cuenta siguiendo unos sencillos pasos.
-    Haz clic en el siguiente enlace de activación
+    ¿Necesitas cambiar tu contraseña?
+    Haz clic en el siguiente enlace para recuperarla
     {os.getenv('FRONT_URL')}recover-password/{url}
     """
 
