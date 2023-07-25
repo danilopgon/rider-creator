8e7ac6d1ac4fa0fe2d768eee9340da4027dcd3e0
Daniel López González <dani.lopezgonzalez.94@gmail.com>
2023-07-25 21:06:44 +0200

    Fix app routing

--- api/templates_email/activation_account.py
+++ api/templates_email/activation_account.py
@@ -7,7 +7,7 @@ def email_activation_text(username="", url=""):
     ¡Bienvenido/a a [Nombre de la empresa/organización]!
     Estamos encantados de que hayas decidido unirte a nuestra comunidad. Para comenzar a aprovechar al máximo nuestros servicios, es necesario que actives tu cuenta siguiendo unos sencillos pasos.
     Haz clic en el siguiente enlace de activación
-    {os.getenv('FRONT_URL')}activation/{url}
+    {os.getenv('BACK_URL')}activation/{url}
     """
 
 
@@ -23,7 +23,7 @@ def msg_activation(url="", username=""):
                  Estamos encantados de que hayas decidido unirte a nuestra comunidad. Para comenzar a aprovechar al máximo nuestros servicios, es necesario que actives tu cuenta siguiendo unos sencillos pasos.
             </p>
             <p style="text-align:center; color: white">Haz clic en el siguiente enlace de activación</p>
-            <a href="{f"{os.getenv('FRONT_URL')}activation/{url}"}" style="text-decoration:none; border:1px; padding: 8px; background:#641AE6; border-radius:10px;color:white; text-align: center; ">Activa tu cuenta</a>
+            <a href="{f"{os.getenv('BACK_URL')}activation/{url}"}" style="text-decoration:none; border:1px; padding: 8px; background:#641AE6; border-radius:10px;color:white; text-align: center; ">Activa tu cuenta</a>
             <p> Si no te has registrado, ignora este correo. </p>
             <p> Si no puedes acceder al enlace, copia y pega la siguiente dirección en tu navegador: {f"{os.getenv('FRONT_URL')}activation/{url}"}</p>
     </body>
