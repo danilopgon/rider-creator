478afda6a9007c6c252c44c9b4e736e71cb31bd9
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-07-28 09:32:27 +0000

    validacion token dash

--- api/app.py
+++ api/app.py
@@ -25,7 +25,7 @@ static_file_dir = os.path.join(
 
 # init mail app
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-app.config["MAIL_PORT"] = os.getenv("MAIL_PORT", 465)
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS") == "True"
