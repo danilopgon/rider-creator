e01f3a04e39a8cca8a9d8e446278c442b200cd6b
Daniel López González <dani.lopezgonzalez.94@gmail.com>
2023-07-28 20:39:30 +0200

    Toaster configuration as component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 
 import "./App.css";
 import { LoginProvider } from "./context/LoginContext";
 import { AppProvider } from "./context/AppContext";
 import NavBar from "./components/NavBar";
+import MainToaster from "./components/MainToaster";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Dashboard from "./views/Dashboard";
@@ -17,28 +17,7 @@ import RiderCreation from "./views/RiderCreation";
 function App() {
   return (
     <BrowserRouter basename="/">
-      <Toaster
-        position="top-center"
-        gutter={8}
-        containerClassName="toast rounded-xl"
-        containerStyle={{}}
-        toastOptions={{
-          className: "bg-base-300 text-base-content",
-          duration: 5000,
-          success: {
-            duration: 3000,
-            className: "bg-success text-success-content",
-            iconTheme: {
-              primary: "hsl(var(--suc))",
-              secondary: "#fff",
-            },
-          },
-          error: {
-            duration: 3000,
-            className: "bg-error text-error-content",
-          },
-        }}
-      />
+      <MainToaster />
       <LoginProvider>
         <AppProvider>
           <NavBar />
