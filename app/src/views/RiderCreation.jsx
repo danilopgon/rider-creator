513f1a51a72c6e4532dc64fd7596b03bf64184c8
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-07-28 08:52:44 +0000

    actualizacion estilos

--- app/src/views/RiderCreation.jsx
+++ app/src/views/RiderCreation.jsx
@@ -30,8 +30,8 @@ const RiderCreation = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-full max-w-md">
-        <h1 className="text-center mb-4 text-3xl font-bold">Crea tu rider</h1>
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="text-center mb-4 text-3xl font-bold text-gray-700">Crea tu rider</h1>
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
@@ -47,7 +47,7 @@ const RiderCreation = () => {
                 </label>
                 <Field
                   type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="banda"
                   name="banda"
                   placeholder="Nombre de la banda"
@@ -67,7 +67,7 @@ const RiderCreation = () => {
                 </label>
                 <Field
                   type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="sala"
                   name="sala"
                   placeholder="Nombre de la sala"
@@ -87,7 +87,7 @@ const RiderCreation = () => {
                 </label>
                 <Field
                   type="date"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="fecha"
                   name="fecha"
                 />
@@ -114,55 +114,4 @@ const RiderCreation = () => {
 };
 
 export default RiderCreation;
-import "tailwindcss/tailwind.css";
 
-const RiderCreation = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-md">
-        <h1 className="text-center mb-4 text-3xl font-bold">Crea tu rider</h1>
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <form className="flex flex-col">
-            <div className="mb-4">
-              <label htmlFor="exampleInputText" className="form-label">
-                Selecciona tu banda
-              </label>
-              <input
-                type="text"
-                className="form-input"
-                id="exampleInputText"
-                aria-describedby="Select your band"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="exampleInputText2" className="form-label">
-                ¿En qué sala?
-              </label>
-              <input
-                type="text"
-                className="form-input"
-                id="exampleInputText2"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="exampleInputDate" className="form-label">
-                Fecha
-              </label>
-              <input type="date" className="form-input" id="exampleInputDate" />
-            </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                ¡Comienza a crear!
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default RiderCreation;
