fda2a7b2fecd2e7607f523b1d16c8162b4d49dca
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-07-29 09:22:43 +0000

    colores

--- app/src/views/RiderCreation.jsx
+++ app/src/views/RiderCreation.jsx
@@ -36,91 +36,88 @@ const RiderCreation = () => {
           "url(https://images.pexels.com/photos/210922/pexels-photo-210922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)",
       }}
     >
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-md">
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
-        <h1 className="text-center mb-4 text-3xl font-bold  text-neutral-focus">Crea tu rider</h1>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            <Form className="flex flex-col">
-              <div className="mb-4">
-                <label
-                  htmlFor="banda"
-                  className="block text-neutral-focus text-sm font-bold mb-2"
-                >
-                  Selecciona tu banda
-                </label>
-                <Field
-                  type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-neutral-focus leading-tight focus:outline-none focus:shadow-outline"
-                  id="banda"
-                  name="banda"
-                  placeholder="Nombre de la banda"
-                />
-                <ErrorMessage
-                  name="banda"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="sala"
-                  className="block text-neutral-focus text-sm font-bold mb-2"
-                >
-                  ¿En qué sala?
-                </label>
-                <Field
-                  type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-neutral-focus leading-tight focus:outline-none focus:shadow-outline"
-                  id="sala"
-                  name="sala"
-                  placeholder="Nombre de la sala"
-                />
-                <ErrorMessage
-                  name="sala"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="fecha"
-                  className="block text-neutral-focus text-sm font-bold mb-2"
-                >
-                  Fecha
-                </label>
-                <Field
-                  type="date"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="fecha"
-                  name="fecha"
-                />
-                <ErrorMessage
-                  name="fecha"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
-              </div>
-              <div className="text-center">
-                <button
-                  type="submit"
-                  className="bg-violet-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                  ¡Comienza a crear!
-                </button>
-              </div>
-            </Form>
-          </Formik>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-full max-w-md">
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+            <h1 className="text-center mb-4 text-3xl font-bold  text-neutral-focus">Crea tu rider</h1>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              <Form className="flex flex-col">
+                <div className="mb-4">
+                  <label
+                    htmlFor="banda"
+                    className="block text-neutral-focus text-sm font-bold mb-2"
+                  >
+                    Selecciona tu banda
+                  </label>
+                  <Field
+                    type="text"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-neutral-focus leading-tight focus:outline-none focus:shadow-outline"
+                    id="banda"
+                    name="banda"
+                    placeholder="Nombre de la banda"
+                  />
+                  <ErrorMessage
+                    name="banda"
+                    component="div"
+                    className="text-red-500 text-sm"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="sala"
+                    className="block text-neutral-focus text-sm font-bold mb-2"
+                  >
+                    ¿En qué sala?
+                  </label>
+                  <Field
+                    type="text"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-neutral-focus leading-tight focus:outline-none focus:shadow-outline"
+                    id="sala"
+                    name="sala"
+                    placeholder="Nombre de la sala"
+                  />
+                  <ErrorMessage
+                    name="sala"
+                    component="div"
+                    className="text-red-500 text-sm"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="fecha"
+                    className="block text-neutral-focus text-sm font-bold mb-2"
+                  >
+                    Fecha
+                  </label>
+                  <Field
+                    type="date"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="fecha"
+                    name="fecha"
+                  />
+                  <ErrorMessage
+                    name="fecha"
+                    component="div"
+                    className="text-red-500 text-sm"
+                  />
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="bg-violet-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    ¡Comienza a crear!
+                  </button>
+                </div>
+              </Form>
+            </Formik>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  );
-};
-
+  )};
 export default RiderCreation;
-
