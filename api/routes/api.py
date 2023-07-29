9aef1df1c11dfeb1b741b718dd262fee270a9a0a
Daniel López González <dani.lopezgonzalez.94@gmail.com>
2023-07-29 12:08:59 +0200

    Correct endpoint api config

--- api/routes/api.py
+++ api/routes/api.py
@@ -6,4 +6,4 @@ from .default_gear_route import default_gear
 api = Blueprint("api", __name__)
 
 api.register_blueprint(user_auth, url_prefix="/auth")
-api.register_blueprint(default_gear, url_prefix="/config-default-gear")
+api.register_blueprint(default_gear, url_prefix="/default-gear")
