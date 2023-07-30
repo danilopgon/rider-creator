b9222c21ba23079db905242cf5535064ef2d5ba3
michelcub <orestelopez96@gmail.com>
2023-07-30 13:54:34 +0100

    add loginc in form

--- app/src/components/CardUserBand.jsx
+++ app/src/components/CardUserBand.jsx
@@ -1,11 +1,11 @@
-export const CardUserBand = ({ user }) => {
+export const CardUserBand = ({ member }) => {
     return(
-        <article className="flex rounded bg-slate-50 p-1 items-center h-14">
+        <article className="flex rounded bg-slate-50 p-1 items-center h-14" key={member.id}>
             <div className="w-10 h-10 flex items-center justify-center">
-                <img className="rounded-full border h-full w-full" src={user.image} alt={user.name} />
+                <img className="rounded-full border h-full w-full" src={""} alt={member.name} />
             </div>
             <div className="flex flex-col items-center">
-                <h3 className="ms-4">First name</h3>
+                <h3 className="ms-4">{member.name}</h3>
                 <p className="ms-4">instrument</p>
             </div>
             <div className="ms-auto">
