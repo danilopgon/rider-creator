4d0f22e3bc4636408c87057944a58f6315a9ee30
michelcub <orestelopez96@gmail.com>
2023-08-08 18:43:01 +0100

    fix error

---
+++ api/controllers/create_rider_controller.py
@@ -0,0 +1,15 @@
+
+
+
+def create_rider_controller():
+    pass
+
+
+
+
+def get_all_riders_controller():
+    pass
+
+
+def update_rider_controller():
+    pass
--- api/routes/create_band_route.py
+++ api/routes/create_band_route.py
@@ -8,24 +8,14 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 create_band = Blueprint('create_band', __name__)
 
 @create_band.route('/', methods=['POST', 'GET','PUT'])
-@jwt_required
 def band():
     if request.method == 'POST':
-        current_user = get_jwt_identity()
-        if current_user is None:
-            return jsonify({"msg": "Missing Authorization Header, token is empty"}), 401
         return create_band_controller()
     
     if request.method == 'GET':
-        current_user = get_jwt_identity()
-        if current_user is None:
-            return jsonify({"msg": "Missing Authorization Header, token is empty"}), 401
         return get_all_bands_controller()
     
     if request.method == 'PUT':
-        current_user = get_jwt_identity()
-        if current_user is None:
-            return jsonify({"msg": "Missing Authorization Header, token is empty"}), 401
         return update_band_controller()
     
     
@@ -33,26 +23,15 @@ def band():
 
     
 @create_band.route('/<id>', methods=['GET', 'DELETE'])
-@jwt_required
 def band_by_id(id):
     if request.method == 'GET':
-        current_user = get_jwt_identity()
-        if current_user is None:
-            return jsonify({"msg": "Missing Authorization Header, token is empty"}), 401
         return get_band_by_id_controller(id)
     if request.method == 'DELETE':
-        current_user = get_jwt_identity()
-        if current_user is None:
-            return jsonify({"msg": "Missing Authorization Header, token is empty"}), 401
         return delete_band_by_id_controller(id)
     
 @create_band.route('/name/<name>', methods=['GET'])
-@jwt_required
 def band_by_name(name):
     if request.method == 'GET':
-        current_user = get_jwt_identity()
-        if current_user is None:
-            return jsonify({"msg": "Missing Authorization Header, token is empty"}), 401
         return get_band_by_name_controller(name)
     
 @create_band.route('/band_by_user', methods=['GET'])
