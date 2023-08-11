99560e320c541d959327232207a87ee691a021b6
michelcub <orestelopez96@gmail.com>
2023-08-12 00:11:21 +0100

    add animation

--- app/src/components/bandCreator/stepFinal.jsx
+++ app/src/components/bandCreator/stepFinal.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const StepFinal = () => {
     return (
         <div className="w-full h-full bg-slate-300 sm:w-[80%] lg:w-[80%] xl:w-[90%] mx-auto flex flex-col items-center p-1 rounded animate-fade-left">
-      <div className="w-[30%] flex justify-center">
+      <div className="w-[30%] flex justify-center animate-pulse">
         <Lottie animationData={check}/>
       </div>
       <h2 className="my-0 text-4xl font-semibold text-center text-black">
